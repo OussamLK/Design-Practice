d7793b29f8c187ff4929e7326969400e17052d70
Oussam <larkem.m.oussam@gmail.com>
2022-06-20 21:28:40 +0100

    add a pop-min function to use in the huffman algorithm

--- huffman_encoding/huffman_tree.rkt
+++ huffman_encoding/huffman_tree.rkt
@@ -5,11 +5,17 @@
 (define (get-frequency node)
     (if (eq? (car node) 'leaf) (caddr node)
         (+ (get-frequency (cadr node)) (get-frequency (caddr node)))))
+(define (pop-min elements key) ; key is a function as in Python3
+    (let ((smallest (apply min (map key elements)))
+          (head (car elements)))
+          (if (= (key head) smallest) (cdr elements) (cons head (pop-min (cdr elements) key)))))
 
 
 
-;tests
+;Tests
 (let ((l1 (make-leaf 'a 2))
       (l2 (make-leaf 'b 1)))
-      (assert (= (get-frequency (make-node l1 l2)) 3) "get-frequency not working properly" ))
+      (assert (= (get-frequency (make-node l1 l2)) 3) "get-frequency not working properly" )
+      (assert (equal? (pop-min '((3 2) (1 2) (10 1) (9 2)) car)
+                      '((3 2) (10 1) (9 2))) "pop-min not working properly" ))
 
